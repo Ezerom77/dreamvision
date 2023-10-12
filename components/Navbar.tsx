49bd7bad5d608ba59d67249609cb3dd33c21b9f2
Ezequiel Romera <eze@EzM1.local>
2023-10-12 19:53:44 -0300

    testing mobile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 py-5 text-white">
+    <nav className="flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 py-5 text-white text-center">
       <div className="flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px8 sm:px-16">
         <Link href="/">
           <Image src="/ai-logo.png" alt="Logo" width={80} height={50} />
@@ -18,8 +18,8 @@ const Navbar = () => {
             height={30}
           />
         </div> */}
-        <ul className="flex-center gap-x-3 block md:flex ml-3">
-          <li className="body-text text-gradient_blue-purple font-bold">
+        <ul className="flex-center gap-x-3 block md:flex ml-3 ">
+          <li className="body-text text-gradient_blue-purple font-bold ">
             <Link href="access">EARLY ACCESS</Link>
           </li>
           <li className="body-text font-normal">
